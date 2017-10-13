a57ee5fb7e11f2031fc13012898a08ad30a3b958
pimukthee <pimukthee@gmail.com>
2017-10-13 21:42:22 +0700

    create table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->integer('total_follower')->default(0);
+            $table->integer('total_following')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
@@ -32,4 +35,3 @@ class CreateUsersTable extends Migration
     {
         Schema::dropIfExists('users');
     }
-}
