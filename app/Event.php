7ae42408230a41c0cc376cc449e22c983d385d5c
pimukthee <pimukthee@gmail.com>
2017-10-21 15:41:28 +0700

    add fillable for Event

--- app/Event.php
+++ app/Event.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    protected $fillable = ['name', 'start_date', 'location', 'min_rank', 'description'];
     public function user() {
         return $this->belongsTo(User::class, 'user_id');
     }
