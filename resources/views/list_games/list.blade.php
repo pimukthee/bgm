3cd63d8180293b2c1474de3e1b53d895d4ecd0aa
thatchayut <thatchayut.u@gmail.com>
2017-10-22 23:28:31 +0700

    change to using bullet point.

--- resources/views/list_games/list.blade.php
+++ resources/views/list_games/list.blade.php
@@ -7,16 +7,13 @@
             @foreach($categories as $category)
                 <div class="card text-center">
                     <div class="card-block">
-                        <h4 class="card-title">{{$category -> game_type}}</h4>
+                        <h1 class="card-title">{{$category -> game_type}}</h1>
                         @foreach($games as $game)
                             @if($game -> categories_id == $category -> id )
-                                <h1>{{$game -> name}}</h1>
+                                <li>{{$game -> name}}</li>
                             @endif 
                         @endforeach
                     </div>
-                    <!-- <div class="card-footer">
-                        @Html.ActionLink("Permits", "Index", "Home", new { Area = "Permit" }, new { @class = "btn btn-primary" })
-                    </div> -->
                 </div>
             @endforeach
         </div>
