aef69e8c25908c95bb2b3b15ab7e65959c424b90
thatchayut <thatchayut.u@gmail.com>
2017-10-22 17:38:21 +0700

    Create game_table

---
+++ database/migrations/2017_10_22_053130_create_game_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('categories_id');
+            $table->string('icon');
+            $table->string('cover_picture');
+            $table->string('rule');
+            $table->string('ranking_rule');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
