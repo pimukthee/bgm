49b145d8f9868afe76d8ea004b9a602ad61583e3
thatchayut <thatchayut.u@gmail.com>
2017-10-22 17:41:02 +0700

    include seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call('UsersTableSeeder');
         $this->call('EventsTableSeeder');
+        $this->call('GameTableSeeder');
+        $this->call('CategoriesTableSeeder');
     }
 }
