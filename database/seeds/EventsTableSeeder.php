f24305821e94a626cd9de80e469e4065869365f8
thatchayut <thatchayut.u@gmail.com>
2017-10-23 22:45:16 +0700

    - add event to check asc/desc

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -31,5 +31,27 @@ class EventsTableSeeder extends Seeder
             database table Create a migration for the session database table',
             'start_date' => '2008-11-11'
         ]);
+
+        DB::table('events')->insert([
+            'id' => '0003',
+            'name' => 'yu_gi_oh_game_01',
+            'user_id' => '3',
+            'game_id' => '4',
+            'location' => '521',
+            'description' => 'Create a migration for the session 
+            database table Create a migration for the session database table',
+            'start_date' => '2008-11-10'
+        ]);
+
+        DB::table('events')->insert([
+            'id' => '0004',
+            'name' => 'summoners_game_01',
+            'user_id' => '3',
+            'game_id' => '3',
+            'location' => 'maya',
+            'description' => 'Create a migration for the session 
+            database table Create a migration for the session database table',
+            'start_date' => '2017-11-10'
+        ]);
     }
 }
