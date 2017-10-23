82b66f41dd7c5fad167c0d46580e1fc40e322fd6
pimukthee <pimukthee@gmail.com>
2017-10-23 19:08:33 +0700

    add ranks  table seeder

---
+++ database/seeds/RanksTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RanksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('ranks')->insert([
+            'user_id' => '1',
+            'game_id' => '2',
+            'score' => '100'
+        ]);
+        DB::table('ranks')->insert([
+            'user_id' => '1',
+            'game_id' => '2',
+            'score' => '292'
+        ]); 
+        DB::table('ranks')->insert([
+            'user_id' => '2',
+            'game_id' => '4',
+            'score' => '544'
+        ]); 
+        DB::table('ranks')->insert([
+            'user_id' => '1',
+            'game_id' => '4',
+            'score' => '231'
+        ]); 
+        DB::table('ranks')->insert([
+            'user_id' => '2',
+            'game_id' => '2',
+            'score' => '123'
+        ]); 
+    }
+}
