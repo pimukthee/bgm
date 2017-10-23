f11919b728ebbcd645dd8f07c8c94bd1af470369
thatchayut <thatchayut.u@gmail.com>
2017-10-23 14:55:20 +0700

    - change position of category's box - add anchor for each game name

--- resources/views/list_games/list.blade.php
+++ resources/views/list_games/list.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 
     <div class="container">
-        <div class="card-deck">
+        <div class="card-deck" style="margin-top: -200px" >
             @foreach($categories as $category)
                 <div class="card text-center">
                     <div class="card-block">
                         <h1 class="card-title">{{$category -> game_type}}</h1>
                         @foreach($games as $game)
                             @if($game -> categories_id == $category -> id )
-                                <li>{{$game -> name}}</li>
+                                <li><a href=#>{{$game -> name}}</a></li>
                             @endif 
                         @endforeach
                     </div>
