73e3bba9993d04a60c7edb3c16e92714aab2ccc7
pimukthee <pimukthee@gmail.com>
2017-11-20 01:40:07 +0700

    add required to login page

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -10,27 +10,27 @@
 
         <label for="name">Name: </label>
         <div class="form-group">
-            <input type="text" class="form-control" id="registerName" placeholder="Name" name="name">
+            <input type="text" class="form-control" id="registerName" placeholder="Name" name="name" required>
         </div>
 
         <label for="username">Username:</label>
         <div class="form-group">
-            <input type="username" class="form-control" id="username" placeholder="Username" name="username">
+            <input type="username" class="form-control" id="username" placeholder="Username" name="username" required>
         </div>
 
         <label for="email">Email: </label>
         <div class="form-group">
-            <input type="text" class="form-control" id="email" placeholder="Email" name="email">
+            <input type="text" class="form-control" id="email" placeholder="Email" name="email" required>
         </div>
 
         <label for="password">Password: </label>
         <div class="form-group">
-            <input type="password" class="form-control" id="password" placeholder="Password" name="password">
+            <input type="password" class="form-control" id="password" placeholder="Password" name="password" required>
         </div>
 
         <label for="password_confirmation">Password Confirmation: </label>
         <div class="form-group">
-            <input type="password" class="form-control" id="password_confirmation" placeholder="Confirm Password" name="password_confirmation">
+            <input type="password" class="form-control" id="password_confirmation" placeholder="Confirm Password" name="password_confirmation" required>
         </div>
 
         <div class="form-group">
