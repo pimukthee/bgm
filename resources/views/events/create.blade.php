da40a92b63335366375e6d1ff141220656316d48
pimukthee <pimukthee@gmail.com>
2017-11-20 01:38:42 +0700

    add required when create event

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -9,18 +9,18 @@
        {{csrf_field()}}
         <div class="form-group">
             <label for="game">Game Name</label>
-            <input type="text" class="form-control" id="game" name="game_name">
+            <input type="text" class="form-control" id="game" name="game_name" required>
         </div>
 
         <div class="form-group">
             <label for="event">Event Name</label>
-            <input type="text" class="form-control" id="event" name="name">
+            <input type="text" class="form-control" id="event" name="name" required>
         </div>
 
          <div class="form-group">    
             <label for="datePicket">Date</label>
             <div class="input-group date">
-                <input type="text" class="form-control" id="datePicker" name="start_date">
+                <input type="text" class="form-control" id="datePicker" name="start_date" required>
             </div>
             <p id="dateHelpBlock" class="form-text text-muted">
                 yyyy-mm-dd hh:mm
@@ -29,12 +29,12 @@
 
          <div class="form-group">
           <label for="inputdefault">Location</label>
-          <input class="form-control" id="inputdefault" type="text" name="location">
+          <input class="form-control" id="inputdefault" type="text" name="location" required>
          </div>
 
          <div class="form-group">
           <label for="inputdefault">Require ranking</label>
-          <input class="form-control" id="inputdefault" type="text" name="min_rank">
+          <input class="form-control" id="inputdefault" type="text" name="min_rank" required>
          </div>
 
          <div class="form-group">
