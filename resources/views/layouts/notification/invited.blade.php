7914242f61e469da99ea5c6e7ddeb5903d2626a4
pimukthee <pimukthee@gmail.com>
2017-12-13 22:15:36 +0700

    added open invite message new tab

--- resources/views/layouts/notification/invited.blade.php
+++ resources/views/layouts/notification/invited.blade.php
@@ -1,3 +1,3 @@
-<a class="dropdown-item" href="/events/{{$notification->data['event']['id']}}">
+<a class="dropdown-item" href="/events/{{$notification->data['event']['id']}}" target="_blank">
     {{$notification->data['user']['name']}} invited you to join <strong>{{$notification->data['event']['name']}}</strong>
 </a>
