0cff119d52665fc147bacacce16936afa84694b3
Kawewut Chujit <kawewut.chu@gmail.com>
2017-12-13 23:43:24 +0700

    Added DB Config

--- database/seeds/GameTableSeeder.php
+++ database/seeds/GameTableSeeder.php
@@ -17,7 +17,7 @@ class GameTableSeeder extends Seeder
             'categories_id' => '1',
             'icon' => 'some icon',
             'cover_picture' => 'some cover',
-            'rule' => 'Lorem ipsum dolor sit amet.',
+            'rule' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
             'ranking_rule' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
         ]); 
 
@@ -27,7 +27,7 @@ class GameTableSeeder extends Seeder
             'categories_id' => '1',
             'icon' => 'some icon',
             'cover_picture' => 'some cover',
-            'rule' => 'Lorem ipsum dolor sit amet.',
+            'rule' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
             'ranking_rule' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
         ]); 
 
@@ -37,7 +37,7 @@ class GameTableSeeder extends Seeder
             'categories_id' => '2',
             'icon' => 'some icon',
             'cover_picture' => 'some cover',
-            'rule' => 'Lorem ipsum dolor sit amet.',
+            'rule' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
             'ranking_rule' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
         ]); 
 
@@ -47,7 +47,7 @@ class GameTableSeeder extends Seeder
             'categories_id' => '2',
             'icon' => 'some icon',
             'cover_picture' => 'some cover',
-            'rule' => 'Lorem ipsum dolor sit amet.',
+            'rule' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum',
             'ranking_rule' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' 
         ]); 
         
