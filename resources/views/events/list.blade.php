231b37acf9bfbc269bafd0b7443f2f32adc31733
Kawewut Chujit <kawewut.chu@gmail.com>
2017-12-13 20:38:00 +0700

    fixed invite btn

--- resources/views/events/list.blade.php
+++ resources/views/events/list.blade.php
@@ -15,10 +15,12 @@
                 <div class="col-md-auto">
                     <h5>DATE : {{$event->start_date}}</h5>
                         <div class="row">
-                            <div class="col-md-10">
+                            <div class="col-md-11">
                                 <p>Required Rank: {{$event->min_rank}}</p>
                                 <p>Max participants: {{$event->max_participants}}</p>
-                                <a class="btn btn-primary" href="/users/{{auth()->id()}}/invite" role="button">INVITE</a>
+                                @if (auth()->check())
+                                    <a class="btn btn-primary" href="/users/{{auth()->id()}}/invite" role="button">INVITE</a>
+                                @endif
                             </div>
                         </div>    
                 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,68 +42,75 @@
     </div>
   <!-- homepage carousel end -->
   @foreach ($events as $event)
-  <a href="/events/{{$event->id}}" style="color:black; text-decoration: none;">
-  <div class="row card-space" href= "/events/$event->id">
-      <div class="col-1"></div>
-          <div class="card col-10">
-          <div class="row">
-              <div class="col">
-                  <h4><b>EVENT: {{$event->name}}</b></h4>
-                  @if(strlen($event->description) != 0)
-                    <p>{{substr($event->description, 0, 20)}}...</p>
-                  @endif
-                  <h5>Location: {{$event->location}}</h5>
-                  <h5>Recruit: {{($event->max_participants)-($event->number)}} player(s)</h5>
-              </div>
-              <div class="col-md-auto">
-                  <h5>DATE : {{$event->start_date}}</h5>
-                      <div class="row">
-                          <div class="col-md-11">
-                          <p>Required Rank: {{$event->min_rank}}</p>
-                          <p>Max participants: {{$event->max_participants}}</p>
-                          <a class="btn btn-primary" href="/users/{{auth()->id()}}/invite" role="button">INVITE</a>
-                          </div>
+ 
+    <div class="row card-space" href= "/events/$event->id">
+        <div class="col-1"></div>
+            <div class="card col-10">
+            <div class="row">
 
-                      </div>    
+                <div class="col">
+                <a href="/events/{{$event->id}}" style="color:black; text-decoration: none;">
+                    <h4><b>EVENT: {{$event->name}}</b></h4>
+                    @if(strlen($event->description) != 0)
+                        <p>{{substr($event->description, 0, 20)}}...</p>
+                    @endif
+                    <h5>Location: {{$event->location}}</h5>
+                    <h5>Recruit: {{($event->max_participants)-($event->number)}} player(s)</h5>  
+                </a>
+                </div>
+                <div class="col-md-auto">
+                    <h5>DATE : {{$event->start_date}}</h5>
+                        <div class="row">
+                            <div class="col-md-11">
+                            <p>Required Rank: {{$event->min_rank}}</p>
+                            <p>Max participants: {{$event->max_participants}}</p>
+                            @if (auth()->check())
+                                <a class="btn btn-primary" href="/users/{{auth()->id()}}/invite" role="button">INVITE</a>
+                            @endif
+                            </div>
+                        </div>    
               </div>
               <div class="col col-lg-3">
-                  <h4><b>Host: {{$event->user->name}}</b></h4>
-                  @if (auth()->check() && !in_array($event->id, $participatedEvents))
-                              @if (($event->num) < ($event->max_participants))
-                                  <form method="post" action="/join/{{$event->id}}">
-                                      {{ csrf_field() }}
-                                      <input type="hidden" name="event_id" value="{{$event->id}}">
-                                      <button class="btn btn-secondary" type="submit">JOIN</button>
-                                  </form> 
-                              @endif    
-                                  <form method="get" action="/events/{{$event->id}}/participants">
-                                      {{ csrf_field() }}
-                                      <input type="hidden" name="event_id" value="{{$event->id}}">
-                                      <button class="btn btn-secondary" type="submit">PARTICIPANTS</button>
-                                  </form>
-                                  @if (auth()->id() == $event->user_id)
-                                      <form method="post" action="/events/delete/{{$event->id}}">
+                    <h4><b>Host: {{$event->user->name}}</b></h4>
+                    @if (auth()->check() && !in_array($event->id, $participatedEvents))
+                                @if (($event->num) < ($event->max_participants))
+                                    <form method="post" action="/join/{{$event->id}}">
+                                        {{ csrf_field() }}
+                                        <input type="hidden" name="event_id" value="{{$event->id}}">
+                                        <button class="btn btn-secondary" type="submit">JOIN</button>
+                                    </form> 
+                                @endif    
+                                    <form method="get" action="/events/{{$event->id}}/participants">
+                                        {{ csrf_field() }}
+                                        <input type="hidden" name="event_id" value="{{$event->id}}">
+                                        <button class="btn btn-secondary" type="submit">PARTICIPANTS</button>
+                                    </form>
+                                @if (auth()->id() == $event->user_id)
+                                    <form method="post" action="/events/delete/{{$event->id}}">
                                           {{ csrf_field() }}
                                           <input type="hidden" name="event_id" value="{{$event->id}}">
                                           <button class="btn btn-danger" type="submit">DELETE</button>
-                                      </form>
-                                  @endif
-                              @endif
+                                    </form>
+                                @endif
+                    @endif
 
-                              @if (auth()->check() && in_array($event->id, $participatedEvents))
-                                  <form method="post" action="/events/cancel/{{$event->id}}">
-                                      {{ csrf_field() }}
-                                      <input type="hidden" name="event_id" value="{{$event->id}}">
-                                      <button class="btn btn-danger" type="submit">CANCEL</button>
-                                  </form>
-                              @endif
-                  <p><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span></p>
-              </div>
-          </div>
-          </div>
-      <div class="col-1"></div>
-  </div>
-    </a>
+                    @if (auth()->check() && in_array($event->id, $participatedEvents))
+                        <form method="post" action="/events/cancel/{{$event->id}}">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="event_id" value="{{$event->id}}">
+                            <button class="btn btn-danger" type="submit">CANCEL</button>
+                        </form>
+                    @endif
+                    <p><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span><span class="glyphicon glyphicon-star"></span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-1"></div>
+    
+    
+
+
   @endforeach
     <div class="card-footer">
         <a href="/events" class="btn btn-primary btn-lg btn-block" role="button" aria-pressed="true">See More</a>
